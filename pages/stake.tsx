8d7a6007313a693387de974bb76a407fff235d2a
Abhijeet Ranjan <velocitybytecode@gmail.com>
2023-01-09 02:24:49 +0530

    first commit

---
+++ pages/stake.tsx
@@ -0,0 +1,182 @@
+import {
+  ThirdwebNftMedia,
+  useAddress,
+  useContract,
+  useContractWrite,
+  useMetamask,
+  useOwnedNFTs,
+  useTokenBalance,
+} from "@thirdweb-dev/react";
+import { NFT } from "@thirdweb-dev/sdk";
+import { BigNumber, ethers } from "ethers";
+import type { NextPage } from "next";
+import { useEffect, useState } from "react";
+import {
+  nftDropContractAddress,
+  stakingContractAddress,
+  tokenContractAddress,
+} from "../consts/contractAddresses";
+import styles from "../styles/Home.module.css";
+
+const Stake: NextPage = () => {
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+  const { contract: nftDropContract } = useContract(
+    nftDropContractAddress,
+    "nft-drop"
+  );
+  const { contract: tokenContract } = useContract(
+    tokenContractAddress,
+    "token"
+  );
+  const { contract, isLoading } = useContract(stakingContractAddress);
+  const { data: ownedNfts } = useOwnedNFTs(nftDropContract, address);
+  const { data: tokenBalance } = useTokenBalance(tokenContract, address);
+  const { mutateAsync: claimRewards } = useContractWrite(
+    contract,
+    "claimRewards"
+  );
+  const [stakedNfts, setStakedNfts] = useState<NFT[]>([]);
+  const [claimableRewards, setClaimableRewards] = useState<BigNumber>();
+
+  useEffect(() => {
+    if (!contract) return;
+
+    async function loadStakedNfts() {
+      const stakedTokens = await contract?.call("getStakeInfo", address);
+
+      // For each staked token, fetch it from the sdk
+      const stakedNfts = await Promise.all(
+        stakedTokens[0]?.map(async (stakedToken: BigNumber) => {
+          const nft = await nftDropContract?.get(stakedToken.toNumber());
+          return nft;
+        })
+      );
+
+      setStakedNfts(stakedNfts);
+    }
+
+    if (address) {
+      loadStakedNfts();
+    }
+  }, [address, contract, nftDropContract]);
+
+  useEffect(() => {
+    if (!contract || !address) return;
+
+    async function loadClaimableRewards() {
+      const stakeInfo = await contract?.call("getStakeInfo", address);
+      setClaimableRewards(stakeInfo[1]);
+    }
+
+    loadClaimableRewards();
+  }, [address, contract]);
+
+  async function stakeNft(id: string) {
+    if (!address) return;
+
+    const isApproved = await nftDropContract?.isApproved(
+      address,
+      stakingContractAddress
+    );
+    if (!isApproved) {
+      await nftDropContract?.setApprovalForAll(stakingContractAddress, true);
+    }
+    await contract?.call("stake", [id]);
+  }
+
+  async function withdraw(id: string) {
+    await contract?.call("withdraw", [id]);
+  }
+
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.h1}>Stake Your NFTs</h1>
+      <hr className={`${styles.divider} ${styles.spacerTop}`} />
+
+      {!address ? (
+        <button className={styles.mainButton} onClick={connectWithMetamask}>
+          Connect Wallet
+        </button>
+      ) : (
+        <>
+          <h2>Your Tokens</h2>
+          <div className={styles.tokenGrid}>
+            <div className={styles.tokenItem}>
+              <h3 className={styles.tokenLabel}>Claimable Rewards</h3>
+              <p className={styles.tokenValue}>
+                <b>
+                  {!claimableRewards
+                    ? "Loading..."
+                    : ethers.utils.formatUnits(claimableRewards, 18)}
+                </b>{" "}
+                {tokenBalance?.symbol}
+              </p>
+            </div>
+            <div className={styles.tokenItem}>
+              <h3 className={styles.tokenLabel}>Current Balance</h3>
+              <p className={styles.tokenValue}>
+                <b>{tokenBalance?.displayValue}</b> {tokenBalance?.symbol}
+              </p>
+            </div>
+          </div>
+
+          <button
+            className={`${styles.mainButton} ${styles.spacerTop}`}
+            onClick={() => claimRewards([])}
+          >
+            Claim Rewards
+          </button>
+
+          <hr className={`${styles.divider} ${styles.spacerTop}`} />
+          <h2>Your Staked NFTs</h2>
+          <div className={styles.nftBoxGrid}>
+            {stakedNfts?.map((nft) => (
+              <div className={styles.nftBox} key={nft.metadata.id.toString()}>
+                {nft?.metadata && (
+                  <ThirdwebNftMedia
+                    metadata={nft.metadata}
+                    className={styles.nftMedia}
+                  />
+                )}
+                <h3>{nft ? (nft.metadata.name) : {}}</h3>
+                <button
+                  className={`${styles.mainButton} ${styles.spacerBottom}`}
+                  onClick={() => withdraw(nft.metadata.id)}
+                >
+                  Withdraw
+                </button>
+              </div>
+            ))}
+          </div>
+
+          <hr className={`${styles.divider} ${styles.spacerTop}`} />
+          <h2>Your Unstaked NFTs</h2>
+          <div className={styles.nftBoxGrid}>
+            {ownedNfts?.map((nft) => (
+              <div className={styles.nftBox} key={nft.metadata.id.toString()}>
+                <ThirdwebNftMedia
+                  metadata={nft.metadata}
+                  className={styles.nftMedia}
+                />
+                <h3>{nft.metadata.name}</h3>
+                <button
+                  className={`${styles.mainButton} ${styles.spacerBottom}`}
+                  onClick={() => stakeNft(nft.metadata.id)}
+                >
+                  Stake
+                </button>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Stake;
